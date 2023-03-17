6624d3b94092a6f60ab7bd74b617838f09ee660b
Ogong <67952526+dbsgur@users.noreply.github.com>
2023-03-17 10:00:00 +0900

    feat: chat route

--- apps/diary-server/src/routes/diary/chat/index.ts
+++ apps/diary-server/src/routes/diary/chat/index.ts
@@ -1,7 +1,10 @@
 import Router from '@koa/router';
+import { post } from './post';
 
 const router = new Router({
     prefix: '/chat',
 });
+
+router.use(post.routes());
 
 export { router as chatRouter };
