f1bdcd027db2133f1945d887ef5f1097610c9b70
Ogong <67952526+dbsgur@users.noreply.github.com>
2023-03-17 10:00:00 +0900

    feat: speech route

--- apps/diary-server/src/routes/diary/index.ts
+++ apps/diary-server/src/routes/diary/index.ts
@@ -1,14 +1,12 @@
 import Router from '@koa/router';
 import { chatRouter } from './chat';
 import { imagesRouter } from './images';
-import { post } from './post';
 import { speechRouter } from './speech';
 
 const router = new Router({
     prefix: '/diary',
 });
 
-router.use(post.routes());
 router.use(speechRouter.routes());
 router.use(chatRouter.routes());
 router.use(imagesRouter.routes());
