d9119718f8c2258af369a2b9149a4caa119d4299
Ogong <67952526+dbsgur@users.noreply.github.com>
2023-12-19 10:00:00 +0900

    fix: tts repository

--- apps/diary-server/src/services/tts/infrastructure/repository.ts
+++ apps/diary-server/src/services/tts/infrastructure/repository.ts
@@ -1,9 +1,19 @@
 import path from 'path';
+import OpenAI from 'openai';
 import { Service } from 'typedi';
 
 @Service()
 export class TTSRepository {
+    private _client: any;
+
     path!: string;
+
+    private get client() {
+        if (!this._client) {
+            this._client = new OpenAI();
+        }
+        return this._client;
+    }
 
     constructor() {
         // TODO: path 설정
