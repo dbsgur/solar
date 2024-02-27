56a7ef51908aa0d5d0bc709a9caa0c602d043c60
Ogong <kkr431@naver.com>
2024-02-27 18:20:59 +0900

    FEAT: package util

---
+++ packages/utils/index.ts
@@ -0,0 +1,23 @@
+export function stripUndefined(obj: { [key: string]: any }) {
+  const stripped = Object.keys(obj).reduce((acc: { [key: string]: any }, prop) => {
+    if (obj[prop] !== undefined) {
+      acc[prop] = obj[prop];
+    }
+    return acc;
+  }, {});
+  if (Object.keys(stripped).length === 0) {
+    return undefined;
+  }
+  return stripped;
+}
+
+export function conversionRate(numerator: number, denominator: number) {
+  if (denominator === 0) {
+    return '-';
+  }
+  return String(Number((numerator / denominator) * 100).toFixed(2));
+}
+
+export function strippedUnchanged(object: Record<string, any>, changedObject: Record<string, any>) {
+  return Object.fromEntries(Object.entries(changedObject).filter(([key, value]) => object[key] !== value));
+}
