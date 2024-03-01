2e2fd46a33142ccdf79a3233215da7e3067440e1
dbsgur <kkr431@naver.com>
2024-03-01 16:29:26 +0900

    FEAT: solar - server +++ koa rate limit

--- solar/template/server/src/index.ts
+++ solar/template/server/src/index.ts
@@ -1,4 +1,4 @@
-import Koa from 'koa';
+import Koa, { Context } from 'koa';
 import koaBody from 'koa-body';
 import {
     errorHandlerMiddleware,
@@ -7,9 +7,30 @@ import {
 } from '../middleware';
 import { router } from './routes';
 import GracefulShutdown from 'http-graceful-shutdown';
+import KoaRatelimit from 'koa-ratelimit';
 
 (async () => {
     const app = new Koa();
+
+    app.use(
+        KoaRatelimit({
+            driver: 'memory',
+            db: new Map(),
+            duration: 60000, // milliseconds
+            errorMessage: 'I hate you. I shoot solar beam~!☀️☀️☀️☀️',
+            id: (ctx) => ctx.ip,
+            headers: {
+                remaining: 'Rate-Limit-Remaining',
+                reset: 'Rate-Limit-Reset',
+                total: 'Rate-Limit-Total',
+            },
+            max: 50,
+            disableHeader: false,
+            // TODO: whitelist, blacklist는 나중에 사용해보자
+            // whitelist: (ctx: Context) => {},
+            // blacklist: (ctx: Context) => {},
+        }),
+    );
     // TODO: 포트 바꿔라잉
     const PORT = 6;
     app.use(uuidMiddleware);
