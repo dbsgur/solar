d46ab0b6677c9218c7c7cf7f39626fb18da06ca2
dbsgur <kkr431@naver.com>
2024-03-01 15:44:26 +0900

    UPDATE: package config ...

--- packages/date/src/index.ts
+++ packages/date/src/index.ts
@@ -2,145 +2,198 @@ import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 import advanced from "dayjs/plugin/advancedFormat";
-import duration from "dayjs/plugin/duration";
-import customParserFormat from "dayjs/plugin/customParseFormat";
+
+type CalendarDate = string;
+type DateTime = string;
+type DayOfWeek =
+  | "Sunday"
+  | "Monday"
+  | "Tuesday"
+  | "Wednesday"
+  | "Thursday"
+  | "Friday"
+  | "Saturday"
+  | "AnyDay";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(advanced);
-dayjs.extend(duration);
-dayjs.extend(customParserFormat);
 
-type DateTime = string;
+export function today(): CalendarDate {
+  return dayjs(new Date()).format("YYYY-MM-DD");
+}
 
-export function now(): DateTime {
-  return dayjs().toISOString();
+export function startOfMonth(date: CalendarDate): CalendarDate {
+  return dayjs(date).startOf("month").format("YYYY-MM-DD");
+}
+
+export function endOfMonth(date: CalendarDate): CalendarDate {
+  return dayjs(date).endOf("month").format("YYYY-MM-DD");
+}
+
+export function nextCalendarDate(date: CalendarDate): CalendarDate {
+  return dayjs(date).add(1, "day").format("YYYY-MM-DD");
+}
+
+export function addMonths(date: CalendarDate, count: number): CalendarDate {
+  return dayjs(date).add(count, "month").format("YYYY-MM-DD");
+}
+
+export function setDate(
+  calendarDate: CalendarDate,
+  dayOfMonth: number
+): CalendarDate {
+  const [year, month] = calendarDate.split("-");
+
+  return new Date(Date.UTC(Number(year), Number(month) - 1, dayOfMonth))
+    .toISOString()
+    .split("T")[0];
+}
+
+export function startOfDay(date: CalendarDate, timezone: string): Date {
+  return dayjs.tz(date, timezone).startOf("day").toDate();
+}
+
+export function endOfDay(date: CalendarDate, timezone: string): Date {
+  return dayjs.tz(date, timezone).endOf("day").toDate();
+}
+
+/**
+ * Date of month
+ */
+export function date(date: CalendarDate): number {
+  return Number(date.split("-")[2]);
 }
 
 export function format({
   date,
-  timezone,
-  template,
+  format,
 }: {
-  date: DateTime;
-  timezone?: string;
-  template: string;
-}): string {
-  return dayjs.utc(date).tz(timezone).format(template);
+  date: CalendarDate;
+  format:
+    | "YYYY-MM-DD"
+    | "MM/DD/YYYY"
+    | "YYYY-MM"
+    | "MMM, YYYY"
+    | "YYYYMMDD"
+    | "MM-DD-YYYY"
+    | "MMDDYY"
+    | "M/DD/YYYY"
+    | "MMDD"
+    | "MMMM YYYY"
+    | "MMMM, YYYY"
+    | "MMMM D"
+    | "MMMM"
+    | "MMMM-YYYY"
+    | "YYYY"
+    | "MM/YYYY"
+    | "MM/DD/YYYY(ddd)";
+}): CalendarDate {
+  return dayjs(date).format(format);
 }
 
-export function startOfDay({
-  date,
-  timezone,
-}: {
-  date: DateTime;
-  timezone?: string;
-}) {
-  return dayjs(date).tz(timezone).startOf("day").toISOString();
+export function differenceInMonths(
+  dateLeft: CalendarDate,
+  dateRight: CalendarDate
+): number {
+  const yearDiff =
+    new Date(dateLeft).getFullYear() - new Date(dateRight).getFullYear();
+  return (
+    new Date(dateLeft).getMonth() +
+    12 * yearDiff -
+    new Date(dateRight).getMonth()
+  );
 }
 
-export function endOfDay({
-  date,
-  timezone,
-}: {
-  date: DateTime;
-  timezone?: string;
-}) {
-  return dayjs(date).tz(timezone).endOf("day").toISOString();
+export function addDays(
+  calendarDate: CalendarDate,
+  count: number
+): CalendarDate {
+  return dayjs(calendarDate).add(count, "days").format("YYYY-MM-DD");
 }
 
-export function startOfMonth({
-  date,
-  timezone,
-}: {
-  date: DateTime;
-  timezone?: string;
-}) {
-  return dayjs(date).tz(timezone).startOf("month").toISOString();
+export function differenceInDays(
+  dateLeft: CalendarDate,
+  dateRight: CalendarDate
+): number {
+  return (
+    (new Date(dateLeft).valueOf() - new Date(dateRight).valueOf()) /
+    (1000 * 60 * 60 * 24)
+  );
 }
 
-export function endOfMonth({
-  date,
-  timezone,
-}: {
-  date: DateTime;
-  timezone?: string;
-}) {
-  return dayjs(date).tz(timezone).endOf("month").toISOString();
+export function add(
+  date: CalendarDate,
+  count: number,
+  unit: "day" | "week" | "month" | "year"
+): CalendarDate {
+  return dayjs(date).add(count, unit).format("YYYY-MM-DD");
 }
 
-export function subtract({
-  date,
-  value,
-  unit,
-}: {
-  date: DateTime;
-  value: number;
-  unit: "day";
-}) {
-  return dayjs(date).subtract(value, unit).toISOString();
+export function parse(date: string | number, timezone?: string): CalendarDate {
+  return dayjs(new Date(date)).tz(timezone).format("YYYY-MM-DD");
 }
 
-export function isAfter(date1: DateTime, date2: DateTime): boolean {
-  return dayjs(date1).isAfter(date2);
+export function toISOString(
+  calendarDate: CalendarDate,
+  timezone: string
+): DateTime {
+  return dayjs.tz(calendarDate, timezone).toISOString();
 }
 
-export function isBefore(date1: DateTime, date2: DateTime) {
+export function isBefore(date1: CalendarDate, date2: CalendarDate): boolean {
   return dayjs(date1).isBefore(date2);
 }
 
-export function getIntervalFromDate(date: DateTime) {
-  return Math.floor(new Date(date).valueOf() / (30 * 60 * 1000));
+/**
+ * @returns  0 (Sunday) to 6 (Saturday)
+ */
+export function dayOfWeek(date: CalendarDate): number {
+  return dayjs(date).day();
 }
 
-export function getDateFromInterval(interval: number) {
-  return dayjs.utc(interval * 30 * 60 * 1000).toISOString();
+export function toDayOfWeekString(date: CalendarDate): DayOfWeek {
+  return (
+    [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ] as DayOfWeek[]
+  )[dayjs(date).day()];
+}
+
+export function day(date: CalendarDate, day: number): string {
+  return dayjs(date).day(day).format("YYYY-MM-DD");
 }
 
 /**
- * startDate와 endDate 사이의 기간을 unit 단위로 리턴한다. \
- * range를 [)로 계산한다. ex) 4월 1일 ~ 4월 3일이면 startDate: 2022-04-01T00:00:00.000Z, endDate: 2022-04-04T00:00:00.000Z로 해야함
+ * 다음 요일까지 남은 일자를 반환한다.
  */
-export function getDateRangeAsTimeUnit(
-  startDate: DateTime,
-  endDate: DateTime,
-  unit: "hour" | "day" | "week" | "month",
-  timezone: string = "America/Los_Angeles"
+export function nextDayDiff(
+  initialDate: CalendarDate,
+  dayOfWeeks: DayOfWeek[]
 ) {
-  const startAt = dayjs(startDate).tz(timezone).startOf(unit);
-  // XXX: [)로 들어오는 건 일 단위라 주 단위 차이나 월 단위 차이는 계산하기 이상해져서 []로 바꾼다.
-  const endAt = dayjs(endDate).tz(timezone).endOf(unit);
-  const periodDiff = endAt.diff(startAt, unit) + 1;
-
-  return Array(periodDiff)
-    .fill("")
-    .map((_, index) => startAt.add(index, unit).startOf(unit));
+  const date = dayjs.tz(initialDate, "UTC").toDate();
+  const days: DayOfWeek[] = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const initialDay = days[date.getDay()];
+  const sortedDays = [
+    ...days.slice(date.getDay()),
+    ...days.slice(undefined, date.getDay()),
+  ];
+  const result = sortedDays.findIndex((day) => {
+    return initialDay !== day && dayOfWeeks.includes(day as DayOfWeek);
+  });
+  return result === -1 ? 7 : result;
 }
-
-// NOTE: startAt - endAt 사이의 시간을 unit 단위로 리턴
-export const getTimeDiff = (
-  startAt: DateTime,
-  endAt: DateTime,
-  unit?: "second" | "minute" | "day"
-): number => {
-  return dayjs(endAt).diff(startAt, unit);
-};
-
-// NOTE: 단위 시간을 지정 포맷으로 변환해서 리턴
-export const getDuration = (time: number, unit: "minute", format: "HH:mm") =>
-  dayjs.duration(time, unit).format(format);
-
-// NOTE: timezone short name // e.g PDT or PDT (UTC-07:00)
-export function formatTimezone(
-  timezone: string,
-  format: "z" | "z (UTCZ)"
-): string {
-  return dayjs().tz(timezone).format(format);
-}
-
-// NOTE: 특정 포맷은 크롬에서는 잘 동작하지만, Safari 에서는 동작하지 않을 수 있으므로 포맷을 명시해주어야한다.
-export const getDateWithTimezone = (
-  date: dayjs.ConfigType,
-  timezone: string,
-  options?: { keepLocalTime?: boolean; format?: string }
-) => dayjs(date, options?.format).tz(timezone, options?.keepLocalTime);
