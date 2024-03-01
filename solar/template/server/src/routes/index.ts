6681df908cf524fad7d249b1ffa3778155be0607
dbsgur <kkr431@naver.com>
2024-03-01 17:14:43 +0900

    chore: before monorepo test

--- solar/template/server/src/routes/index.ts
+++ solar/template/server/src/routes/index.ts
@@ -3,6 +3,8 @@ import { Context } from 'koa';
 
 export const router = new Router();
 
+// TODO: USE Hapi/Joi
+
 router.get('/ping', (ctx: Context) => {
-  ctx.body = 'pong';
+    ctx.body = 'pong';
 });
