4327c064b543ec4ce584f8de8b7aac077afeeb20
Ogong <kkr431@naver.com>
2024-03-04 16:08:29 +0900

    UPDATE: diary-client setting

--- apps/diary-client/webpack.config.js
+++ apps/diary-client/webpack.config.js
@@ -1,35 +1,41 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const resolve = require('./webpack.config.resolve');
 
-module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: { loader: require.resolve('ts-loader'), },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      favicon: "./src/assets/favicon/example.ico",
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
+module.exports = () => {
+  return {
+    resolve,
+    entry: './src/index.tsx',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/',
     },
-    compress: true,
-    port: 3000,
-  },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: { loader: require.resolve('ts-loader') },
+        },
+        {
+          test: /\.css$/, // .css 확장자를 가진 파일에 적용될 로더 설정
+          use: ['style-loader', 'css-loader'], // 사용할 로더 설정
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        favicon: './src/assets/favicon/example.ico',
+      }),
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist'),
+      },
+      compress: true,
+      port: 3000,
+    },
+  };
 };
