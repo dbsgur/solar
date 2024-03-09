4091b1257c90e842af918b4a93fb4311072e5ab2
Ogong <kkr431@naver.com>
2024-03-09 16:06:22 +0900

    chore: react strict mode

--- apps/diary-client/src/index.tsx
+++ apps/diary-client/src/index.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
+import React from 'react';
 
 // Render your React component instead
 const root = createRoot(document.getElementById('app')!);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
