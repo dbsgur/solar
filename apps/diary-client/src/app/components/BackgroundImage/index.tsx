a3f2232dc6bb3be1ef0b01ce4a5447315310d5a6
Ogong <kkr431@naver.com>
2024-03-10 15:43:08 +0900

    fix: background-image imageUrl props add

--- apps/diary-client/src/app/components/BackgroundImage/index.tsx
+++ apps/diary-client/src/app/components/BackgroundImage/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
-function BackGroundImage(props: { children?: React.ReactNode }) {
+function BackGroundImage(props: {
+  children?: React.ReactNode;
+  imageUrl: string;
+}) {
   // prop destruction
-  const { children } = props;
+  const { children, imageUrl } = props;
   // lib, style hooks
   // state, ref hooks
   // query hooks
   // calculated values
   // effects
   // handlers
-  const imageUrl = '';
   // 'https://images.unsplash.com/photo-1528722828814-77b9b83aafb2?q=80&w=4639&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
   const style = {
-    // backgroundImage: `url(${imageUrl})`,
+    backgroundImage: `url(${imageUrl})`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
