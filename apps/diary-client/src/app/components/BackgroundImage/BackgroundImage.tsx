2332e054d191d8d6d1dd627d3d92928f4cee5854
Ogong <kkr431@naver.com>
2024-03-10 17:19:36 +0900

    REFACTOR: children -> Outlet in component

--- apps/diary-client/src/app/components/BackgroundImage/BackgroundImage.tsx
+++ apps/diary-client/src/app/components/BackgroundImage/BackgroundImage.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
-function BackGroundImage(props: {
-  children?: React.ReactNode;
-  imageUrl: string;
-}) {
+function BackGroundImage(props: { imageUrl: string }) {
   // prop destruction
-  const { children, imageUrl } = props;
+  const { imageUrl } = props;
   // lib, style hooks
   // state, ref hooks
   // query hooks
   // calculated values
   // effects
   // handlers
-  // 'https://images.unsplash.com/photo-1528722828814-77b9b83aafb2?q=80&w=4639&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+
   const style = {
     backgroundImage: `url(${imageUrl})`,
     backgroundSize: 'cover',
@@ -21,9 +19,14 @@ function BackGroundImage(props: {
     width: '100%',
     height: '100%',
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    // ...css,
   };
 
-  return <div css={style}>{children}</div>;
+  return (
+    <div css={style}>
+      <Outlet />
+    </div>
+  );
 }
 
 export { BackGroundImage };
