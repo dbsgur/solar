f4a1752036298cb3375f075ca79649221e00cecb
Ogong <kkr431@naver.com>
2024-03-20 13:56:23 +0900

    fix: lint

--- apps/diary-client/.eslintrc.js
+++ apps/diary-client/.eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  extends: ["airbnb", "prettier"],
-  plugins: ["prettier"],
+  extends: ['airbnb', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
   },
 };
