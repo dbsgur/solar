64e37bb25dc9d249b6905336f89eb2205f3c952f
Ogong <kkr431@naver.com>
2024-03-23 15:34:25 +0900

    chore: eslint file

--- solar/template/csr/.eslintrc.js
+++ solar/template/csr/.eslintrc.js
@@ -1,9 +1,14 @@
 module.exports = {
+  parser: '@typescript-eslint/parser',
   extends: ['airbnb', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
     'react/react-in-jsx-scope': 'off',
     'import/prefer-default-export': 'off',
+    'react/no-unknown-property': ['error', { ignore: ['css'] }],
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
   },
 };
