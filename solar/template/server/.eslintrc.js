20e564b4c995ae7cf2a63921c5796bf028f391e3
Ogong <67952526+dbsgur@users.noreply.github.com>
2024-03-26 16:10:12 +0900

    fix: template-server setting

--- solar/template/server/.eslintrc.js
+++ solar/template/server/.eslintrc.js
@@ -1,7 +1,51 @@
 module.exports = {
-    extends: ["airbnb", "prettier"],
-    plugins: ["prettier"],
+    parser: '@typescript-eslint/parser',
+    extends: ['airbnb', 'prettier'],
+    plugins: ['prettier'],
     rules: {
-        "prettier/prettier": "error",
+        'no-console': 'off',
+        'no-unused-vars': 'warn',
+        'prettier/prettier': 'error',
+        'import/prefer-default-export': 'off',
+        'import/no-unresolved': 'error',
+        'react/no-unknown-property': [
+            'error',
+            { ignore: ['css', 'className'] },
+        ],
+        'arrow-body-style': 'off',
+        'import/extensions': [
+            'error',
+            'always',
+            {
+                js: 'never',
+                ts: 'never',
+            },
+        ],
+        'import/order': [
+            'error',
+            {
+                groups: [
+                    'builtin',
+                    'external',
+                    'internal',
+                    ['parent', 'sibling', 'index'],
+                ],
+                alphabetize: {
+                    order: 'asc',
+                    caseInsensitive: true,
+                },
+            },
+        ],
+    },
+    parserOptions: {
+        ecmaVersion: 2021,
+    },
+    settings: {
+        'import/resolver': {
+            typescript: { project: './tsconfig.json' },
+        },
+    },
+    env: {
+        node: true,
     },
 };
