0dbfc3edfa31b8669f523acea9d3ac8ca734214d
Ogong <67952526+dbsgur@users.noreply.github.com>
2024-03-28 12:20:03 +0900

    feat: tts model

---
+++ apps/diary-server/src/services/tts/domain/model.ts
@@ -0,0 +1,13 @@
+import { Service } from 'typedi';
+
+@Service()
+export class TTSModel {
+    // constructor(private openAIService: OpenAIService) {}
+    // async generateSpeech(inputText: string) {
+    //     return await this.openAIService.audio.speech.create({
+    //         model: 'tts-1',
+    //         voice: 'alloy',
+    //         input: inputText,
+    //     });
+    // }
+}
