3c7be1c3e10c548e1a365591c60506ed2e8f20a4
Ogong <67952526+dbsgur@users.noreply.github.com>
2024-03-30 15:17:17 +0900

    fix: error handler

--- apps/diary-server/src/middlewares/error-handler.ts
+++ apps/diary-server/src/middlewares/error-handler.ts
@@ -1,15 +1,15 @@
-import { Context } from "koa";
+import { Context } from 'koa';
 
 export const errorHandlerMiddleware = async (
-  ctx: Context,
-  next: () => Promise<any>
+    ctx: Context,
+    next: () => Promise<any>,
 ) => {
-  try {
-    await next();
-  } catch (error){
-    ctx.status = error.status;
-    const { status, body } = error;
-    // status code 400이상은 dev환경에서만 바디를 출력
-    console.error(`[${status} ERROR]:: ${body}`);
-  }
+    try {
+        await next();
+    } catch (error: any) {
+        ctx.status = error.status;
+        const { status, body } = error;
+        // status code 400이상은 dev환경에서만 바디를 출력
+        console.error(`[${status} ERROR]:: ${body}`);
+    }
 };
