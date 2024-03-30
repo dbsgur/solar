436c0fff801a337bbf66eca3005bdc85c4036cd7
Ogong <67952526+dbsgur@users.noreply.github.com>
2024-03-30 15:18:18 +0900

    fix: uuid middleware

--- apps/diary-server/src/middlewares/uuid.ts
+++ apps/diary-server/src/middlewares/uuid.ts
@@ -1,7 +1,10 @@
 import { Context } from 'koa';
 import { v4 as uuidV4 } from 'uuid';
 
-export const uuidMiddleware = async (ctx: Context, next: () => Promise<any>) => {
-  ctx.state.txId = uuidV4();
-  await next();
+export const uuidMiddleware = async (
+    ctx: Context,
+    next: () => Promise<any>,
+) => {
+    ctx.state.txId = uuidV4();
+    await next();
 };
--- apps/diary-server/src/services/tts/application/service.ts
+++ apps/diary-server/src/services/tts/application/service.ts
@@ -1,23 +1,21 @@
 import fs from 'fs';
 import { Service, Inject } from 'typedi';
-import { OpenAIClient } from '../../../libs/openai';
 import { TTSRepository } from '../infrastructure/repository';
 
 @Service()
 export class TTSService {
     @Inject()
-    private openAIClient!: OpenAIClient;
-
+    // private openAIClient!: OpenAIClient;
     @Inject()
     private tTSRepository!: TTSRepository;
 
-    async createFile(inputText: string) {
-        const aiSpeech = await this.generateSpeech(inputText);
-        const buffer = Buffer.from(await aiSpeech.arrayBuffer());
-        await fs.promises.writeFile(this.tTSRepository.getPath(), buffer);
-    }
+    // async createFile(inputText: string) {
+    // const aiSpeech = await this.generateSpeech(inputText);
+    // const buffer = Buffer.from(await aiSpeech.arrayBuffer());
+    // await fs.promises.writeFile(this.tTSRepository.getPath(), buffer);
+    // }
 
-    generateSpeech(inputText: string) {
-        return this.openAIClient.createSpeech(inputText);
-    }
+    // generateSpeech(inputText: string) {
+    // return this.openAIClient.createSpeech(inputText);
+    // }
 }
