425e871a4ba45c24156620e1ed96307e95855598
Ogong <67952526+dbsgur@users.noreply.github.com>
2024-04-02 11:25:14 +0900

    FIX: openai routes

--- apps/diary-server/src/routes/openai/index.ts
+++ apps/diary-server/src/routes/openai/index.ts
@@ -1,12 +1,14 @@
 import Router from '@koa/router';
+import { fineTuneRouter } from './fine-tune';
 import { post } from './post';
+import { ttsRouter } from './tts';
 
 const router = new Router({
     prefix: '/openai',
 });
 
-// TODO: USE Hapi/Joi
-
 router.use(post.routes());
+router.use(fineTuneRouter.routes());
+router.use(ttsRouter.routes());
 
 export { router as openaiRouter };
---
+++ apps/diary-server/src/routes/openai/tts/index.ts
@@ -0,0 +1,7 @@
+import Router from '@koa/router';
+
+const router = new Router({
+    prefix: '/tts',
+});
+
+export { router as ttsRouter };
