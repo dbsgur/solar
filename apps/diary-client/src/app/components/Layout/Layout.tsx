8b480fa29fe8d1a653d6b1f7a861d0812ba2fa24
Ogong <67952526+dbsgur@users.noreply.github.com>
2024-04-07 15:52:23 +0900

    chore

--- apps/diary-client/src/app/components/Layout/Layout.tsx
+++ apps/diary-client/src/app/components/Layout/Layout.tsx
@@ -1,7 +1,8 @@
 import Stack from '@mui/material/Stack';
+import { ReactNode } from 'react';
 import { Contact } from '~components';
 
-function Layout(props: { children: React.ReactNode }) {
+function Layout(props: { children: ReactNode }) {
   // prop destruction
   const { children } = props;
   // lib, style hooks
