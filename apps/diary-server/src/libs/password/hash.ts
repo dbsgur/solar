8889fab6aed52ba1dd137ca566475f2dc20e5c0f
Ogong <67952526+dbsgur@users.noreply.github.com>
2024-04-07 11:07:37 +0900

    FIX: server

--- apps/diary-server/src/libs/password/hash.ts
+++ apps/diary-server/src/libs/password/hash.ts
@@ -1,3 +1,4 @@
 import * as ShaJs from 'sha.js';
 
-export const generateHash = (str: string) => ShaJs('sha512').update(str).digest('hex');
+export const generateHash = (str: string) =>
+    ShaJs('sha512').update(str).digest('hex');
