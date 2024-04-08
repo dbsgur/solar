a6f891698fb841551c965ae0f31a7430f7c06adc
Ogong <67952526+dbsgur@users.noreply.github.com>
2024-04-08 15:53:22 +0900

    FIX: app

--- apps/diary-client/src/app/App.tsx
+++ apps/diary-client/src/app/App.tsx
@@ -1,11 +1,25 @@
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { Suspense } from 'react';
 import { RouterProvider } from 'react-router-dom';
+import { ErrorBoundary, ThemeProvider } from '~components';
 import { AppRouter } from '~routes';
 
+const queryClient = new QueryClient();
+
 function App() {
-  // queryClient
   // Suspense
-  // ErrorBoundary
-  return <RouterProvider router={AppRouter} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ErrorBoundary>
+        {/* TODO: null -> Loading screen */}
+        <Suspense fallback={null}>
+          <ThemeProvider>
+            <RouterProvider router={AppRouter} />;
+          </ThemeProvider>
+        </Suspense>
+      </ErrorBoundary>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
